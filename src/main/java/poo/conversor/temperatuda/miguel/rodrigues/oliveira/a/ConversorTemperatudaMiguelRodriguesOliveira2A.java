ab1c7665d2dcdb5011821ff10d395a0f5f31ed34
2024105070044 <mrodriguea421@gmail.com>
2025-04-29 08:41:47 -0400

    Primeiro Comit

---
+++ src/main/java/poo/conversor/temperatuda/miguel/rodrigues/oliveira/a/ConversorTemperatudaMiguelRodriguesOliveira2A.java
@@ -0,0 +1,8 @@
+package poo.conversor.temperatuda.miguel.rodrigues.oliveira.a;
+
+public class ConversorTemperatudaMiguelRodriguesOliveira2A {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
